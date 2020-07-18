3e92915d931f520a6f8a9f7b5ced613fc19674e4
Fortie40 <elijahmurimi40@gmail.com>
2020-07-18 15:19:36 +0300

    Code refactor

--- app/src/main/java/com/fortie40/words_filtering3/MainActivity.kt
+++ app/src/main/java/com/fortie40/words_filtering3/MainActivity.kt
@@ -8,16 +8,11 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.speech.RecognizerIntent
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.view.inputmethod.EditorInfo
 import android.widget.EditText
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import com.fortie40.words_filtering3.adapters.MainActivityAdapter
 import com.fortie40.words_filtering3.adapters.SearchAdapter
@@ -33,15 +28,11 @@ import kotlinx.android.synthetic.main.view_search.*
 import java.util.*
 
 class MainActivity : AppCompatActivity(), IClickListener, ISearchViewListener {
-    private lateinit var searchView: SearchView
     private lateinit var mainAdapter: MainActivityAdapter
     private lateinit var searchAdapter: SearchAdapter
     private lateinit var names: List<String>
     private lateinit var sharedPref: SharedPreferences
     private lateinit var recent: ArrayList<String>
-    private lateinit var setFocus: MenuItem
-    private lateinit var voiceSearch: MenuItem
-    private lateinit var closee: MenuItem
     private lateinit var moveUp: Animation
     private lateinit var moveDown: Animation
 
@@ -59,71 +50,6 @@ class MainActivity : AppCompatActivity(), IClickListener, ISearchViewListener {
 
         moveUp = AnimationUtils.loadAnimation(this, R.anim.move_up)
         moveDown = AnimationUtils.loadAnimation(this, R.anim.move_down)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-
-        val searchItem = menu!!.findItem(R.id.app_bar_search)
-        val view = searchItem.actionView
-        searchView = view as SearchView
-
-        setFocus = menu.findItem(R.id.set_focus)
-        voiceSearch = menu.findItem(R.id.voice_search)
-        closee = menu.findItem(R.id.closee)
-        val searchClose =
-            searchView.findViewById<ImageView>(androidx.appcompat.R.id.search_close_btn)
-        searchClose.isEnabled = false
-        searchClose.setImageDrawable(null)
-
-        searchView.imeOptions = EditorInfo.IME_ACTION_SEARCH or EditorInfo.IME_FLAG_NO_EXTRACT_UI
-        searchView.maxWidth = Integer.MAX_VALUE
-        searchView.queryHint = getString(R.string.search_name)
-
-        searchView.setOnQueryTextFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                val query = searchView.query
-                hideShowVoiceCloseIcon(query.toString())
-                setFocus.isVisible = false
-                hideNoResultsFound()
-                getRecentSearches()
-                searchAdapter = SearchAdapter(recent, this)
-                names_item.adapter = searchAdapter
-            }
-        }
-
-        searchItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
-            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
-                getRecentSearches()
-                return true
-            }
-
-            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
-                hideNoResultsFound()
-                setFocus.isVisible = false
-                voiceSearch.isVisible = false
-                close.isVisible = false
-                names_item.adapter = mainAdapter
-                return true
-            }
-        })
-
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                searchName(query)
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                if (names_item.adapter == mainAdapter)
-                    return false
-                Log.i(TAG, "changed")
-                hideShowVoiceCloseIcon(newText)
-                return false
-            }
-        })
-
-        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -146,116 +72,12 @@ class MainActivity : AppCompatActivity(), IClickListener, ISearchViewListener {
         }
     }
 
-    override fun onBackPressed() {
-        if (!searchView.isIconified) {
-            searchView.setQuery("", false)
-            searchView.clearFocus()
-            searchView.isIconified = true
-        } else {
-            super.onBackPressed()
-        }
-    }
-
     override fun onResultsClick(position: Int) {
-        //searchView.setQuery(recent[position], true)
-        searchView.clearFocus()
         println("clicked")
     }
 
     override fun onRestoreClick(position: Int) {
-        searchView.setQuery(recent[position], false)
-    }
-
-    private fun getNames() {
-        names = HelperFunctions.getNames()
-
-        mainAdapter =
-            MainActivityAdapter(names)
-        names_item.adapter = mainAdapter
-    }
-
-    private fun searchName(p0: String?) {
-        if (p0.isNullOrEmpty()) {
-            return
-        }
-        HelperFunctions.hideInputMethod(this, search_input_text)
-        searchAdapter = SearchAdapter(arrayListOf(), this)
-        search_item.adapter = searchAdapter
-        hideShowVoiceCloseIcon()
-        hideNoResultsFound()
-        progressBar.visibility = View.VISIBLE
-        saveToRecentSearch(p0)
-        searchAdapter.originalList = names
-        searchAdapter.string = p0
-        searchAdapter.filter.filter(p0.toLowerCase(Locale.getDefault())) {
-            when(searchAdapter.itemCount) {
-                0 -> {
-                    showNoResultsFound(R.string.no_results_found, text = p0)
-                }
-                else -> {
-                    hideNoResultsFound()
-                }
-            }
-
-            Log.i("MainActivity", p0)
-            if (searchAdapter.string == "" || searchAdapter.string == p0) {
-                Log.i("MainActivity", "done")
-                progressBar.visibility = View.GONE
-                searchView.clearFocus()
-            }
-        }
-    }
-
-    private fun saveToRecentSearch(name: String) {
-        val queries = sharedPref[QUERY, ""]?.split(",")
-
-        val queryList = HelperFunctions.listToArrayList(queries, name = name)
-        if (queryList.size == 6)
-            queryList.removeAt(5)
-
-        val query = HelperFunctions.arrayListToString(queryList)
-
-        sharedPref[QUERY] = query
-    }
-
-    private fun getRecentSearches() {
-        val queries = sharedPref[QUERY, ""]?.split(",")
-        val queryList = HelperFunctions.listToArrayList(queries)
-        Log.i(TAG,"$queryList")
-
-        recent = queryList
-        if (recent.isEmpty()) {
-            showNoResultsFound(R.string.no_recent_search)
-        } else {
-            recent.add(0, HEADER_TITLE)
-        }
-    }
-
-    private fun hideNoResultsFound() {
-        no_results_found.visibility = View.GONE
-    }
-
-    private fun showNoResultsFound(resource: Int, text: String = "") {
-        no_results_found.visibility = View.VISIBLE
-        if (text.isEmpty()) {
-            no_results_found.text = getString(resource)
-        } else {
-            no_results_found.text = getString(resource, text)
-        }
-    }
-
-    private fun hideShowVoiceCloseIcon(p0: String?) {
-        if (p0 == null) {
-            return
-        }
-        voiceSearch.isVisible = p0.isEmpty()
-        close.isVisible = p0.isNotEmpty()
-    }
-
-    private fun hideShowVoiceCloseIcon() {
-        set_focus.isVisible = true
-        voice_search.isVisible = true
-        close.isVisible = false
+        println("clicked")
     }
 
     override fun onPromptSpeechInput() {
@@ -307,6 +129,89 @@ class MainActivity : AppCompatActivity(), IClickListener, ISearchViewListener {
         val query = view.text.toString()
         searchName(query)
         return super.onSubmitQuery(actionId, view)
+    }
+
+    private fun getNames() {
+        names = HelperFunctions.getNames()
+
+        mainAdapter =
+            MainActivityAdapter(names)
+        names_item.adapter = mainAdapter
+    }
+
+    private fun searchName(p0: String?) {
+        if (p0.isNullOrEmpty()) {
+            return
+        }
+        HelperFunctions.hideInputMethod(this, search_input_text)
+        searchAdapter = SearchAdapter(arrayListOf(), this)
+        search_item.adapter = searchAdapter
+        hideShowVoiceCloseIcon()
+        hideNoResultsFound()
+        progressBar.visibility = View.VISIBLE
+        saveToRecentSearch(p0)
+        searchAdapter.originalList = names
+        searchAdapter.string = p0
+        searchAdapter.filter.filter(p0.toLowerCase(Locale.getDefault())) {
+            when(searchAdapter.itemCount) {
+                0 -> {
+                    showNoResultsFound(R.string.no_results_found, text = p0)
+                }
+                else -> {
+                    hideNoResultsFound()
+                }
+            }
+
+            Log.i("MainActivity", p0)
+            if (searchAdapter.string == "" || searchAdapter.string == p0) {
+                Log.i("MainActivity", "done")
+                progressBar.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun saveToRecentSearch(name: String) {
+        val queries = sharedPref[QUERY, ""]?.split(",")
+
+        val queryList = HelperFunctions.listToArrayList(queries, name = name)
+        if (queryList.size == 6)
+            queryList.removeAt(5)
+
+        val query = HelperFunctions.arrayListToString(queryList)
+
+        sharedPref[QUERY] = query
+    }
+
+    private fun getRecentSearches() {
+        val queries = sharedPref[QUERY, ""]?.split(",")
+        val queryList = HelperFunctions.listToArrayList(queries)
+        Log.i(TAG,"$queryList")
+
+        recent = queryList
+        if (recent.isEmpty()) {
+            showNoResultsFound(R.string.no_recent_search)
+        } else {
+            recent.add(0, HEADER_TITLE)
+        }
+    }
+
+    private fun hideNoResultsFound() {
+        no_results_found.visibility = View.GONE
+    }
+
+    private fun showNoResultsFound(resource: Int, text: String = "") {
+        no_results_found.visibility = View.VISIBLE
+        if (text.isEmpty()) {
+            no_results_found.text = getString(resource)
+        } else {
+            no_results_found.text = getString(resource, text)
+        }
+    }
+
+    private fun hideShowVoiceCloseIcon() {
+        set_focus.isVisible = true
+        voice_search.isVisible = true
+        close.isVisible = false
     }
 
     private fun showSearchAdapter(inputText: EditText) {
